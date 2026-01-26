d94453402f87a1b664c60e3743a489b82aeff7a6
predrim <pedrofsp2@gmail.com>
2026-01-26 16:51:48 -0300

    Reduced screen resolution

--- js/core/globalConfig.js
+++ js/core/globalConfig.js
@@ -1,4 +1,4 @@
-export const CANVAS_WIDTH = 1920;
-export const CANVAS_HEIGHT = 1080;
+export const CANVAS_WIDTH = 1280;
+export const CANVAS_HEIGHT = 720;
 export const WORLD_SCALE = 4;
 export const TILE_SIZE = 64;
